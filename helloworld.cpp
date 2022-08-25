4a8e657b8601a439e3808377985de62650905e2d
Kelson L <kelsonlean@gmail.com>
2022-08-25 00:35:42 -0300

    Hello world de duas formas

---
+++ helloworld.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+/*1) Easy way:
+ *
+ *
+ * int main (){
+
+	std::cout<< "Hello world" <<'\n';
+
+	return 0;
+
+}
+
+*/
+
+
+// 2) Using Functions:
+
+void hello (){
+	std::cout<<"Hello World"<<'\n';
+}
+
+int main (){
+
+	hello();
+
+}
+
